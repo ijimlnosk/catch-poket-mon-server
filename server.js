99a0482b52824e7bd347f8730ee107bf15ec0b59
jinsol <skek298@naver.com>
2025-02-10 15:23:47 +0900

    💡refactor: 서버 주소 추가

--- server.js
+++ server.js
@@ -4,16 +4,27 @@ const userRoutes = require("./src/routes/userRoutes");
 const pokemonRoutes = require("./src/routes/pokemonRoutes");
 const errorHandler = require("./src/utils/errorHandler");
 const cors = require("cors");
+const fs = require("fs");
+const https = require("https");
 
 const app = express();
 const PORT = process.env.PORT || 3300;
 
+const sslOptions = {
+  key: fs.readFileSync(process.env.KEY),
+  cert: fs.readFileSync(process.env.CERT),
+};
+
 const corsOptions = {
-    origin: ["http://localhost:5173", "https://catchpoketmon.soljk.com"],
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,
-    optionsSuccessStatus: 200,
+  origin: [
+    "http://localhost:5173",
+    "https://catchpoketmon.soljk.com",
+    "https://pokeserver.soljk.com",
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+  optionsSuccessStatus: 200,
 };
 
 app.use(cors(corsOptions));
@@ -26,6 +37,6 @@ app.use("/api/data/poke", pokemonRoutes);
 
 app.use(errorHandler);
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+https.createServer(sslOptions, app).listen(PORT, () => {
+  console.log(`${PORT}포트로 HTTPS 서버가 열렸어요!`);
 });
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -2,21 +2,21 @@ const jwt = require("jsonwebtoken");
 const jwtConfig = require("../config/jwt");
 
 module.exports = (req, res, next) => {
-    const apiKey = req.query.apiKey;
-    if (apiKey !== process.env.API_KEY) {
-        return res.status(401).json({ message: "유효하지 않은 API 키입니다." });
-    }
+  const apiKey = req.query.apiKey;
+  if (apiKey !== process.env.API_KEY) {
+    return res.status(401).json({ message: "유효하지 않은 API 키입니다." });
+  }
 
-    const token = req.headers.authorization?.split(" ")[1];
-    if (!token) {
-        return res.status(401).json({ message: "인증이 필요합니다." });
-    }
+  const token = req.headers.authorization?.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({ message: "인증이 필요합니다." });
+  }
 
-    try {
-        const decoded = jwt.verify(token, jwtConfig.secret);
-        req.userId = decoded.userId;
-        next();
-    } catch (error) {
-        res.status(401).json({ message: "유효하지 않은 토큰입니다." });
-    }
+  try {
+    const decoded = jwt.verify(token, jwtConfig.secret);
+    req.userId = decoded.userId;
+    next();
+  } catch (error) {
+    res.status(401).json({ message: "유효하지 않은 토큰입니다." });
+  }
 };
